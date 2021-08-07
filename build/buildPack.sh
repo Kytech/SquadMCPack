ddf29d622859f5ae6247ee9eb7179856fe967759
Kytech <Kytech@users.noreply.github.com>
2021-08-07 00:23:24 -0600

    Add dependency checks to build script

---
+++ build/buildPack.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check for dependencies
+if ! which dos2unix > /dev/null; then
+    >&2 echo "Error: dos2unix not found."
+    >&2 echo "Either install dos2unix manually, or if on Windows, ensure you are"
+    >&2 echo "using the latest version of git for Windows (which should include dos2unix)."
+    exit 1
+fi
+
+if ! which packwiz > /dev/null; then
+    >&2 echo "Error: packwiz not found."
+    >&2 echo "Packwiz can be downloaded from https://github.com/comp500/packwiz"
+    exit 1
+fi
