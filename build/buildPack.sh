3b6127cdfa87d20dbbf0ad5283dbca908f265ed4
Kytech <Kytech@users.noreply.github.com>
2021-08-07 14:16:25 -0600

    Move packwiz output to dist folder

--- build/buildPack.sh
+++ build/buildPack.sh
@@ -14,11 +14,26 @@ if ! which packwiz > /dev/null; then
     exit 1
 fi
 
-script_dir="$(dirname "${BASH_SOURCE[0]}")"
+script_dir="$(cd $(dirname "${BASH_SOURCE[0]}"); pwd)"
 
-rm -rf "$script_dir/dl"
+# Clear download cache
+# rm -rf "$script_dir/dl"
 
-git clone https://github.com/Kytech/CreateTogether.git "$script_dir/dl/basePack"
+# git clone https://github.com/Kytech/CreateTogether.git "$script_dir/dl/basePack"
+
+# Remove files and directories not used in the modpack
 cd "$script_dir/dl/basePack"
-
 rm -rf .git/ .github/ automation/ changelogs/ server_files/ .gitattributes .gitignore .prettierrc *.sh *.bat *.jar *.md
+rm -rf packmenu/
+
+# Get list of folders in base modpack
+basePack_dirList=$(find . -maxdepth 1 ! -path . -type d | sed "s|^\./||")
+IFS=$'\n' read -r -a basePack_dirs <<< "$basePack_dirList"
+
+cd "$script_dir/../dist"
+
+# Get modpack directories in dist folder
+repoPack_dirList=$(find . -maxdepth 1 ! -path . -type d | sed "s|^\./||")
+IFS=$'\n' read -r -a repoPack_dirs <<< "$repoPack_dirList"
+
+# packwiz curseforge import "$script_dir/dl/basePack"
---
+++ dist/README.md
@@ -0,0 +1,6 @@
+# Dist folder
+
+Files in this folder should not be edited by hand. These files are managed by the build script.
+
+This folder does not contain a usable minecraft instance by itself. To obtain one, run the build script
+or download from the releases.
