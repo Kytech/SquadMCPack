dbf16fa7e510b5a4a5eeaf8da0ac3c926187de2f
Kytech <Kytech@users.noreply.github.com>
2021-08-07 12:28:50 -0600

    Update build script to download and clean up base modpack

--- build/buildPack.sh
+++ build/buildPack.sh
@@ -3,7 +3,7 @@
 # Check for dependencies
 if ! which dos2unix > /dev/null; then
     >&2 echo "Error: dos2unix not found."
-    >&2 echo "Either install dos2unix manually, or if on Windows, ensure you are"
+    >&2 echo "Either install the dos2unix utility, or, if on Windows, ensure you are"
     >&2 echo "using the latest version of git for Windows (which should include dos2unix)."
     exit 1
 fi
@@ -13,3 +13,12 @@ if ! which packwiz > /dev/null; then
     >&2 echo "Packwiz can be downloaded from https://github.com/comp500/packwiz"
     exit 1
 fi
+
+script_dir="$(dirname "${BASH_SOURCE[0]}")"
+
+rm -rf "$script_dir/dl"
+
+git clone https://github.com/Kytech/CreateTogether.git "$script_dir/dl/basePack"
+cd "$script_dir/dl/basePack"
+
+rm -rf .git/ .github/ automation/ changelogs/ server_files/ .gitattributes .gitignore .prettierrc *.sh *.bat *.jar *.md
