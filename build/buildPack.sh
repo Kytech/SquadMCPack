d6565435a1dbd04b0b4939294a3f835cbe3e5796
Kytech <Kytech@users.noreply.github.com>
2021-08-08 16:51:05 -0600

    Refactor build script

--- build/buildPack.sh
+++ build/buildPack.sh
@@ -21,6 +21,21 @@ fetch_base_pack() {
     rm -rf "$SCRIPT_DIR/dl"
 
     git clone https://github.com/Kytech/CreateTogether.git "$SCRIPT_DIR/dl/basePack"
+
+    pushd "$SCRIPT_DIR/dl/basePack" > /dev/null
+
+    # Remove files and directories excluded from base modpack
+    base_pack_exclude=(".git/" ".github/")
+    IFS=$'\n' read -d '' -a basepack_exclude_file < "$SCRIPT_DIR/../basepack.exclude"
+    base_pack_exclude+=("${basepack_exclude_file[@]}")
+    basepack_exclude_comment_regex='^#'
+    for file in "${base_pack_exclude[@]}"; do
+        if [ ! -z "$file" ] && [[ ! "$file" =~ $basepack_exclude_comment_regex ]]; then
+            rm -rf $file
+        fi
+    done
+
+    popd > /dev/null
 }
 
 display_usage() {
@@ -50,33 +65,20 @@ if [ ! -d "$SCRIPT_DIR/dl/basePack" ] || [ "$1" == "-u" ] || [ "$1" == "--update
     fetch_base_pack
 fi
 
+# Get list of folders that need to be in the modpack
 cd "$SCRIPT_DIR/dl/basePack"
-
-# Remove files and directories excluded from base modpack
-base_pack_exclude=(".git/" ".github/")
-IFS=$'\n' read -d '' -a basepack_exclude_file < "$SCRIPT_DIR/../basepack.exclude"
-base_pack_exclude+=("${basepack_exclude_file[@]}")
-basepack_exclude_comment_regex='^#'
-for file in "${base_pack_exclude[@]}"; do
-    if [ ! -z "$file" ] && [[ ! "$file" =~ $basepack_exclude_comment_regex ]]; then
-        rm -rf $file
-    fi
-done
-
-# Get list of folders in base modpack after cleanup of extra files
 modpack_dir_list="$(find . -maxdepth 1 ! -path . -type d | sed 's|^\./||')"
 IFS=$'\n' read -d '' -a modpack_dirs <<< "$modpack_dir_list"
+cd "$SCRIPT_DIR/.."
+additional_pack_dirs_list="$(find . -maxdepth 1 ! -path . ! -path ./.git ! -path ./build ! -path ./dist  -type d | sed 's|^\./||')"
+IFS=$'\n' read -d '' -a additional_pack_dirs <<< "$additional_pack_dirs_list"
+modpack_dirs+=("${additional_pack_dirs[@]}")
 
 cd "$SCRIPT_DIR/../dist"
 
 # Get names of modpack directories in dist folder
 dist_dir_list="$(find . -maxdepth 1 ! -path . -type d | sed 's|^\./||')"
 IFS=$'\n' read -d '' -a dist_dirs <<< "$dist_dir_list"
-
-# Get names of modpack directories in repo root, excluding build and git folders
-override_dirs_list="$(find .. -maxdepth 1 ! -path .. ! -path ../.git ! -path ../build ! -path ../dist  -type d | sed 's|^\.\./||')"
-IFS=$'\n' read -d '' -a override_dirs <<< "$override_dirs_list"
-modpack_dirs+=("${override_dirs[@]}")
 
 # Remove directories that are no longer in base pack or repo root
 for dir in "${dist_dirs[@]}"; do
@@ -92,9 +94,9 @@ cd "$SCRIPT_DIR/.."
 
 # Normalize file line endings in this repo to lf
 # Copy normalized files to modpack
-for override_dir in "${override_dirs[@]}"; do
+for dir_to_merge in "${additional_pack_dirs[@]}"; do
     # Check to make sure files do not exist before copying
-    files_to_copy_list="$(find ./$override_dir -type f | sed 's|^\./||')"
+    files_to_copy_list="$(find ./$dir_to_merge -type f | sed 's|^\./||')"
     IFS=$'\n' read -d '' -a files_to_copy <<< "$files_to_copy_list"
     for file in "${files_to_copy[@]}"; do
         if [ -f "$SCRIPT_DIR/dl/basePack/$file" ]; then
@@ -106,8 +108,8 @@ for override_dir in "${override_dirs[@]}"; do
         fi
     done
 
-    find "./$override_dir" -type f -exec dos2unix {} \;
-    cp -R "./$override_dir" ./dist/
+    find "./$dir_to_merge" -type f -exec dos2unix {} \;
+    cp -R "./$dir_to_merge" ./dist/
 done
 
 cd "$SCRIPT_DIR/../dist"
